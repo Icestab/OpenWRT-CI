9a6ca16163ba8de380d31eb7b43c016db53f575c
Icestab <44687672+Icestab@users.noreply.github.com>
2025-12-21 10:22:03 +0800

    Add new packages and update existing package entries

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -57,7 +57,10 @@ UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
-UPDATE_PACKAGE "luci-app-ssr-plus" "fw876/helloworld" "master"
+UPDATE_PACKAGE "luci-app-ssr-plus" "fw876/helloworld" "master" "pkg"
+UPDATE_PACKAGE "shadowsocks-libev" "fw876/helloworld" "master" "pkg"
+UPDATE_PACKAGE "shadowsocksr-libev" "fw876/helloworld" "master" "pkg"
+UPDATE_PACKAGE "dns2socks-rust" "fw876/helloworld" "master" "pkg"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
